833aebb87da0051d6b4eda4dc3f452160e25c43d
ifazzzz <shoaibahmedifaz@gmail.com>
2022-09-29 09:15:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Home from './Component/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <Home></Home>
     </div>
   );
 }
---
+++ src/Component/Details/Deatils.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Details.css'
+const Deatils = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Deatils;
