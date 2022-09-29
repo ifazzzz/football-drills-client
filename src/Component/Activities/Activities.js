666d02f6c25a5cc0bdc47452b0ab5b40c52eb0ee
ifazzzz <shoaibahmedifaz@gmail.com>
2022-09-29 10:02:46 +0600

    data loaded

--- src/Component/Activities/Activities.js
+++ src/Component/Activities/Activities.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Activities.css'
-const Activities = () => {
+const Activities = ({contents}) => {
+    const {title , image, description, time} = contents
     return (
         <div>
-            
+            <img src={image} alt="" />
+            <h1 className="text-2xl font-bold font-sans">{title}</h1>
+            <p className="font-sans">{description}</p>
+            <h1 className="text-xl font-bold font-sans">Time Required :{time}</h1>
+            <button className="bg-blue-700 px-6 py-3 rounded-lg text-white">Add to list</button>
         </div>
     );
 };
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import './Home.css'
+import React, { useEffect, useState } from 'react';
+import Activities from '../Activities/Activities';
 const Home = () => {
+
+    const [activities, setActivities] = useState([])
+
+    useEffect(() =>{
+
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setActivities(data))
+
+    }, [])
+
     return (
-        <div>
-            
+        <div className="container mx-auto mt-20">
+            <h1 className="text-2xl font-bold font-sans text-blue-500">FOOTBALL DRILLS CLUB</h1>
+          <div className="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-8">
+            <div className="col-span-3">
+
+                {
+                    activities.map(activity => <Activities 
+                        key ={activity.id}
+                        contents={activity}
+                        ></Activities>)
+                }
+            </div>
+            <div className="">
+                details
+            </div>  
+          </div>
         </div>
     );
 };
